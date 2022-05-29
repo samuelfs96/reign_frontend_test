ba2da8bf4680d102f36d898f9ea54acad1c8668d
samuelfs96 <samuelfs96@gmail.com>
2022-05-29 03:40:59 -0400

    gettimeago helper function

---
+++ src/utils/helpers/getTimeAgo.ts
@@ -0,0 +1,29 @@
+const getTimeAgo = (date: any) => {
+    const _date = (new Date(date)) as any
+    let seconds = Math.floor(((new Date()) as any - _date) / 1000);
+
+    let interval = seconds / 31536000;
+
+    if (interval > 1) {
+        return Math.floor(interval) + " years";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+        return Math.floor(interval) + " months";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+        return Math.floor(interval) + " days";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+        return Math.floor(interval) + " hours";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+        return Math.floor(interval) + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+}
+
+export default getTimeAgo
